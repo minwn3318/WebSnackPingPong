4207ef06ae1efc4bfb1ce7dbe6d33b5304d7c791
minwn3318 <minwn3318@naver.com>
2025-02-04 16:05:09 +0900

    확률 생성표 대거 수정중

--- Assets/Scripts/BlockCounter.cs
+++ Assets/Scripts/BlockCounter.cs
@@ -7,7 +7,7 @@ public class BlockCounter : MonoBehaviour
 {
     [SerializeField] private int intiCount;
     [SerializeField] private int stageGap;
-    [SerializeField] private List<List<List<float>>> probListSelected;
+    [SerializeField] private List<float> probListSelected;
     [SerializeField] private GameManager gameManager;
 
     private void Awake()
@@ -15,57 +15,9 @@ public class BlockCounter : MonoBehaviour
         gameManager = GetComponent<GameManager>();
         intiCount = 8;
         stageGap = 3;
-        initList();
+        probListSelected = new List<float>() {0.3f, 0.4f, 0.5f, 0.6f};
     }
 
-    public void initList()
-    {
-        probListSelected = new List<List<List<float>>>();
-        List<List<float>> probList = new List<List<float>>();
-        List<float> prob = new List<float>();
-
-        probListSelected.Clear();
-        probList.Clear();
-        prob.Clear();
-
-        prob.Add(0.5f);
-        prob.Add(0.5f);
-        probList.Add(prob.ToList());
-
-        prob.Clear();
-
-        prob.Add(0.4f);
-        prob.Add(0.3f);
-        prob.Add(0.3f);
-        probList.Add(prob.ToList());
-        probListSelected.Add(probList.ToList());
-
-        prob.Clear();
-        probList.Clear();
-
-        prob.Add(0.6f);
-        prob.Add(0.4f);
-        probList.Add(prob.ToList());
-
-        prob.Clear();
-
-        prob.Add(0.3f);
-        prob.Add(0.4f);
-        prob.Add(0.4f);
-        probList.Add(prob.ToList());
-        probListSelected.Add(probList.ToList());
-        Debug.Log(probListSelected[0][0][0]);
-        Debug.Log(probListSelected[0][0][1]);
-        Debug.Log(probListSelected[0][1][0]);
-        Debug.Log(probListSelected[0][1][1]);
-        Debug.Log(probListSelected[0][1][2]);
-        Debug.Log(probListSelected[1][0][0]);
-        Debug.Log(probListSelected[1][0][1]);
-        Debug.Log(probListSelected[1][1][0]);
-        Debug.Log(probListSelected[1][1][1]);
-        Debug.Log(probListSelected[1][1][2]);
-
-    }
     public int CaculateCountFromStage()
     {
         int currentStage = (gameManager.Stage / stageGap);
