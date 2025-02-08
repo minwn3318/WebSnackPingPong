905b17936fc68daa697956a7a4da36b5e12741e3
minwn3318 <minwn3318@naver.com>
2025-02-08 14:35:07 +0900

    확률표 선정 함수 완료
    확률표 선정 함수 완료
    화률표에 따라 카운트 뽑는 함수 제작 예정

--- Assets/Scripts/BlockCounter.cs
+++ Assets/Scripts/BlockCounter.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class BlockCounter : MonoBehaviour
 {
     [SerializeField] private int stageGap;
+    [SerializeField] private int lastRemain;
+    [SerializeField] private int forNum;
+    [SerializeField] private int probIndex;
     [SerializeField] private List<float> probList;
     [SerializeField] private List<int> probArray;
     [SerializeField] private GameManager gameManager;
@@ -14,19 +17,36 @@ public class BlockCounter : MonoBehaviour
     {
         gameManager = GetComponent<GameManager>();
         stageGap = 3;
+        forNum = 0;
+        probIndex = 0;
+        lastRemain = -1;
         probList = new List<float>() { 0.5f, 0.5f, 0.4f, 0.3f, 0.3f, 0.4f};
         probArray = new List<int>() { 8, 16, 24, 32, 40};
     }
 
-    public int CaculateCountFromStage()
+    public void SelectProb()
     {
-        int stageState = gameManager.Stage % stageGap;
-        if (stageState != 0)
+        int stageRest = gameManager.Stage % stageGap;
+        switch (stageRest)
         {
-
+            case 0:
+                forNum = 3;
+                break;
+            default:
+                forNum = 2;
+                break;
         }
-        
-        return gameManager.Stage;
+        if(gameManager.Stage < 10 && lastRemain == 0)
+        {
+            probIndex++;
+        }
+        lastRemain = stageRest;
+        if(gameManager.Stage == 10)
+        {
+            probArray.Clear();
+            probArray = new List<int>() { 24, 32, 40 };
+            probIndex = 1;
+        }
     }
 
     float Choose(float[] probs)
