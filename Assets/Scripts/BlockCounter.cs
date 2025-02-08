cd5415317efa405e9df2ee417564ff7f736fc409
minwn3318 <minwn3318@naver.com>
2025-02-08 16:08:19 +0900

    확률 선정 함수 작성 완료
    확률 선정 함수 작성 완료
    스테이지 변경에 따른 변경 디버깅 확인필요

--- Assets/Scripts/BlockCounter.cs
+++ Assets/Scripts/BlockCounter.cs
@@ -8,20 +8,22 @@ public class BlockCounter : MonoBehaviour
     [SerializeField] private int stageGap;
     [SerializeField] private int lastRemain;
     [SerializeField] private int forNum;
+    [SerializeField] private int countIndex;
     [SerializeField] private int probIndex;
     [SerializeField] private List<float> probList;
-    [SerializeField] private List<int> probArray;
+    [SerializeField] private List<int> countList;
     [SerializeField] private GameManager gameManager;
 
     private void Awake()
     {
         gameManager = GetComponent<GameManager>();
         stageGap = 3;
-        forNum = 0;
-        probIndex = 0;
         lastRemain = -1;
-        probList = new List<float>() { 0.5f, 0.5f, 0.4f, 0.3f, 0.3f, 0.4f};
-        probArray = new List<int>() { 8, 16, 24, 32, 40};
+        forNum = 0;
+        countIndex = 0;
+        probIndex = 0;
+        probList = new List<float>() { 0.5f, 0.5f, 0.4f, 0.3f, 0.3f};
+        countList = new List<int>() { 8, 16, 24, 32, 40};
     }
 
     public void SelectProb()
@@ -30,48 +32,54 @@ public class BlockCounter : MonoBehaviour
         switch (stageRest)
         {
             case 0:
-                forNum = 3;
+                forNum = 5;
+                probIndex = 2;
                 break;
             default:
                 forNum = 2;
+                probIndex = 0;
                 break;
         }
         if(gameManager.Stage < 10 && lastRemain == 0)
         {
-            probIndex++;
+            countIndex++;
         }
         lastRemain = stageRest;
         if(gameManager.Stage == 10)
         {
-            probArray.Clear();
-            probArray = new List<int>() { 24, 32, 40 };
-            probIndex = 1;
+            countList.Clear();
+            countList = new List<int>() { 24, 32, 40 };
+            probList.Clear();
+            probList = new List<float>() { 0.6f, 0.4f, 0.4f, 0.3f, 0.3f};
+            countIndex = 1;
         }
     }
 
-    float Choose(float[] probs)
+    int ChooseProbAndCount()
     {
-
         float total = 0;
+        int lastCountIndex = countIndex;
 
-        foreach (float elem in probs)
+        for (int j = probIndex; j < forNum; j++)
         {
-            total += elem;
+            total += probList[j];
         }
-
         float randomPoint = Random.value * total;
 
-        for (int i = 0; i < probs.Length; i++)
+        for (int i = probIndex; i < forNum; i++)
         {
-            if (randomPoint < probs[i])
+            if (randomPoint < probList[i])
             {
-                return i;
+                countIndex = lastCountIndex;
+                return countList[countIndex];
             }
             else
             {
-                randomPoint -= probs[i];
+                randomPoint -= probList[i];
             }
+            countIndex++;
         }
-        return probs.Length - 1;
+        countIndex = lastCountIndex;
+        return forNum;
     }
 }
