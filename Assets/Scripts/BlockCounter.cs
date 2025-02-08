dfb45cdd053fe1f6c6112f1601c517b2c99172c8
minwn3318 <minwn3318@naver.com>
2025-02-08 11:39:51 +0900

    확률표 만들기 성공
    확률표 만들었음
    이제 스테이지에 따른 확률표 고르기
    그리고 골라진 확률표를 저장하고 블럭 카운트 시 무작위로 고르는 함수 제작 필요

--- Assets/Scripts/BlockCounter.cs
+++ Assets/Scripts/BlockCounter.cs
@@ -5,23 +5,53 @@ using UnityEngine;
 
 public class BlockCounter : MonoBehaviour
 {
-    [SerializeField] private int intiCount;
     [SerializeField] private int stageGap;
-    [SerializeField] private List<float> probListSelected;
+    [SerializeField] private List<float> probList;
+    [SerializeField] private List<int> probArray;
     [SerializeField] private GameManager gameManager;
 
     private void Awake()
     {
         gameManager = GetComponent<GameManager>();
-        intiCount = 8;
         stageGap = 3;
-        probListSelected = new List<float>() {0.3f, 0.4f, 0.5f, 0.6f};
+        probList = new List<float>() { 0.5f, 0.5f, 0.4f, 0.3f, 0.3f, 0.4f};
+        probArray = new List<int>() { 8, 16, 24, 32, 40};
     }
 
     public int CaculateCountFromStage()
     {
-        int currentStage = (gameManager.Stage / stageGap);
+        int stageState = gameManager.Stage % stageGap;
+        if (stageState != 0)
+        {
+
+        }
         
         return gameManager.Stage;
+    }
+
+    float Choose(float[] probs)
+    {
+
+        float total = 0;
+
+        foreach (float elem in probs)
+        {
+            total += elem;
+        }
+
+        float randomPoint = Random.value * total;
+
+        for (int i = 0; i < probs.Length; i++)
+        {
+            if (randomPoint < probs[i])
+            {
+                return i;
+            }
+            else
+            {
+                randomPoint -= probs[i];
+            }
+        }
+        return probs.Length - 1;
     }
 }
